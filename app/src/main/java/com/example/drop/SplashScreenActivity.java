3c709cc5f0d3a22e03aebf254763d232dcbd238f
DWave <hirenambekar1@gmail.com>
2019-10-20 01:37:30 +0530

    Splash Screen UI/UX improvement

---
+++ app/src/main/java/com/example/drop/SplashScreenActivity.java
@@ -0,0 +1,63 @@
+package com.example.drop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    ImageView lightwave;
+    ImageView darkwave;
+    ImageView droplogo;
+    TextView droptext;
+    Button startedbutton;
+    Animation frombottom;
+    Animation fadein;
+    Animation fadein2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        lightwave = (ImageView) findViewById(R.id.lightwave);
+        darkwave = (ImageView) findViewById(R.id.darkwave);
+        droplogo = (ImageView) findViewById(R.id.droplogo);
+        droptext = (TextView) findViewById(R.id.droptext);
+        startedbutton = (Button) findViewById(R.id.startedbutton);
+
+        frombottom = AnimationUtils.loadAnimation(this, R.anim.formbottom);
+        fadein = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        fadein2 = AnimationUtils.loadAnimation(this, R.anim.fadein2);
+
+        lightwave.setAnimation(frombottom);
+        darkwave.setAnimation(frombottom);
+
+        droplogo.setAnimation(fadein);
+        droptext.setAnimation(fadein);
+        startedbutton.setAnimation(fadein2);
+
+
+    }
+
+    public void startMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        SplashScreenActivity.this.finish();
+    }
+}
